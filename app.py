fdf5e591aa0e07832133589723496985b1c4088d
Sajid Ahamed <sajid13673@gmail.com>
2026-09-18 15:11:25 +0530

    feat: add CORS middleware

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, UploadFile, File
 from faster_whisper import WhisperModel
+from fastapi.middleware.cors import CORSMiddleware
 import tempfile
 import os
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 print("Loading Whisper model...")
 
